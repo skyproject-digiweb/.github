d0cb4561b29e4635274e198fc0a7d1785e061ae0
Simone Campa <83575839+simocampa95@users.noreply.github.com>
2023-11-28 17:09:27 +0100

    Update install.sh

--- pub/install.sh
+++ pub/install.sh
@@ -26,23 +26,17 @@ repo_name="skyproject-docker-compose"
 
 # Step 2: Clone GitHub repo using the token
 echo "Cloning GitHub repo..."
-git clone https://${github_username}:${github_token}@github.com/skyproject-digiweb/${repo_name}.git
+git clone --depth=1 https://${github_username}:${github_token}@github.com/skyproject-digiweb/${repo_name}.git
 cd ${repo_name}
 chmod +x digiweb.sh
 
 # Step 3: Check Docker daemon and group membership
 if  command_exists docker  &&  check_docker_group ; then
   echo "Docker is installed, running, and the current user is in the 'docker' group."
-
-  if  command_exists keytool  &&  command_exists htpasswd ; then
-    docker login ghcr.io -u ${github_username} -p ${github_token}
-  
-    # Step 4: Launch script inside the cloned repo
-    echo "Launching script inside the cloned repo..."
-    ./digiweb.sh install
-  else
-    echo "'keytool' and 'htpasswd' commands are not installed. Terminating.."
-  fi
+  docker login ghcr.io -u ${github_username} -p ${github_token}
+  # Step 4: Launch script inside the cloned repo
+  echo "Launching script inside the cloned repo..."
+  ./digiweb.sh install
 else
   echo "Docker is not installed, not running, or the current user is not in the 'docker' group."
   echo "Please make sure Docker is set up correctly before running the script."
