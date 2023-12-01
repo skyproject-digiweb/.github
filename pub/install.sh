22a2bcbc22f233455e53a0417f717f44e8433dd2
Stefano Boldrin <143497831+s-boldrin@users.noreply.github.com>
2023-12-01 15:24:49 +0000

    Check deps and make digiweb dir
    Dependecies `git` and `docker` are checked before any action.
    
    Any error exits `1`.
    
    The repository is cloned in a directory named `digiweb`.
    
    Closes Install in digiweb directory #1.

--- pub/install.sh
+++ pub/install.sh
@@ -15,6 +15,25 @@ check_docker_network_create() {
   docker network create skynet
 }
 
+# Step -1: Check dependencies
+# ... git
+if  command_exists git ; then
+  echo "Git is installed"
+else
+  echo "Git is not installed."
+  echo "Please make sure Git is set up correctly before running the script."
+  exit 1
+fi
+
+# ... docker
+if  command_exists docker  &&  check_docker_group ; then
+  echo "Docker is installed, running, and the current user is in the 'docker' group."
+else
+  echo "Docker is not installed, not running, or the current user is not in the 'docker' group."
+  echo "Please make sure Docker is set up correctly before running the script."
+  exit 1
+fi
+
 # Step 0: harcode username
 github_username="s-boldrin"
 
@@ -26,18 +45,17 @@ repo_name="skyproject-docker-compose"
 
 # Step 2: Clone GitHub repo using the token
 echo "Cloning GitHub repo..."
-git clone --depth=1 https://${github_username}:${github_token}@github.com/skyproject-digiweb/${repo_name}.git
-cd ${repo_name}
+git clone --depth=1 https://${github_username}:${github_token}@github.com/skyproject-digiweb/${repo_name}.git digiweb
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+cd digiweb
 chmod +x digiweb.sh
 
-# Step 3: Check Docker daemon and group membership
-if  command_exists docker  &&  check_docker_group ; then
-  echo "Docker is installed, running, and the current user is in the 'docker' group."
-  docker login ghcr.io -u ${github_username} -p ${github_token}
-  # Step 4: Launch script inside the cloned repo
-  echo "Launching script inside the cloned repo..."
-  ./digiweb.sh install
-else
-  echo "Docker is not installed, not running, or the current user is not in the 'docker' group."
-  echo "Please make sure Docker is set up correctly before running the script."
+docker login ghcr.io -u ${github_username} -p ${github_token}
+if [ $? -ne 0 ]; then
+  exit 1
 fi
+
+./digiweb.sh install
+
